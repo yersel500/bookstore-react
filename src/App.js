effbc628ef69ad63bdd4fdeffb96695137c416af
Yersel Hurtado <yersel500@gmail.com>
2022-03-24 12:14:48 -0500

    Add router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import React from 'react';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
 import './App.css';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
+
 
 function App() {
   return (
-    
+  <Router>
+    <nav className="my-nav">
+      <Link to="/" className="my-logo">BookStore</Link>
+      <div className="link-container">
+        <Link className="my-link" to="/">BOOKS</Link>
+        <Link className="my-link" to="/calculator">CATEGORIES</Link>
+      </div>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="*" element={<Books />} />
+    </Routes>
+  </Router>
   );
 }
 
