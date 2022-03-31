49aa65ad3ede1cc01dbd396857182e2600ec3a6f
Yersel Hurtado <yersel500@gmail.com>
2022-03-30 22:33:21 -0500

    Fix comments

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,10 +3,10 @@ import { useSelector } from 'react-redux';
 import Book from './Book';
 
 const BookList = () => {
-  const myState = useSelector((state) => state);
+  const myState = useSelector((state) => state.bookReducer);
   return (
     <div>
-      {myState.add.map((bookDetail) => (
+      {myState.map((bookDetail) => (
         <Book
           details={bookDetail}
           key={bookDetail.id}
