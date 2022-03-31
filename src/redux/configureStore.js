e432b7f58ba7d2db8c845e5e25c57f88cb92849a
Yersel Hurtado <yersel500@gmail.com>
2022-03-31 10:21:33 -0500

    install thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import bookReducer from './books/book';
 import catReducer from './categories/categories';
 
 const rootReducer = combineReducers({ bookReducer, catReducer });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
